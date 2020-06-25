04dc1296e02e6bd1c691d05ebcb3898b6548cf51
ernisklm@gmail.com <ernisklm@gmail.com>
2020-06-25 14:02:12 +0600

    Дополнил с заданиями ДЗ 2.

--- src/Hero.java
+++ src/Hero.java
@@ -2,16 +2,22 @@ public class Hero {
     private int heroHealth;
     private int heroDamage;
     private String heroSuperAbility;
+    private String heroesName;
 
-    public Hero(int heroHealth, int heroDamage, String heroSuperAbility) {
+    public Hero(){}
+
+    public Hero(int heroHealth, int heroDamage, String heroSuperAbility, String heroesName) {
         this.heroHealth = heroHealth;
         this.heroDamage = heroDamage;
         this.heroSuperAbility = heroSuperAbility;
+        this.heroesName = heroesName;
     }
 
-    public Hero (int heroHealth, int heroDamage) {
+    public Hero (int heroHealth, String heroesName, int heroDamage) {
         this.heroHealth = heroHealth;
+        this.heroesName = heroesName;
         this.heroDamage = heroDamage;
+
     }
 
     public int getHeroHealth() {
@@ -24,5 +30,9 @@ public class Hero {
 
     public String getHeroSuperAbility() {
         return heroSuperAbility;
+    }
+
+    public String getHeroesName() {
+        return heroesName;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,32 @@ public class Main {
         Boss boss = new Boss();
         boss.setBossHealth(700);
         boss.setBossDamage(50);
-        boss.setBossDefence("Magic");
+        boss.setBossDefence("Magical");
 
         System.out.println("Boss health: " + boss.getBossHealth());
         System.out.println("Boss damage: " + boss.getBossDamage());
         System.out.println("Boss defence: " + boss.getBossDefence());
+        System.out.println("******************");
+
+        createHeroes();
+    }
+
+    public static Hero[] createHeroes() {
+
+        Hero[] heroes = new Hero[3];
+
+        Hero magic = new Hero(200, 20, "Magical", "Magic");
+        Hero warrior = new Hero(250, "Warrior", 25);
+        Hero kinetic = new Hero(220, 20, "Kinetic", "Kinetic");
+
+        heroes[0] = magic;
+        heroes[1] = warrior;
+        heroes[2] = kinetic;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hero: " + heroes[i].getHeroesName() + "; - Health: " + heroes[i].getHeroHealth() + "; Damage: "
+                    + heroes[i].getHeroDamage() + "; Super ability: " + heroes[i].getHeroSuperAbility() + ";");
+        }
+        return heroes;
     }
 }
